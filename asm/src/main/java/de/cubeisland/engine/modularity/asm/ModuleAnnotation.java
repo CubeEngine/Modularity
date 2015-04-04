b803c99a5f206f2e2e487392926333aa506aed33
Phillip Schichtel <phillip.public@schich.tel>
2015-04-04 22:36:16 +0200

    it... it compiles...

--- asm/src/main/java/de/cubeisland/engine/modularity/asm/ModuleAnnotation.java
+++ asm/src/main/java/de/cubeisland/engine/modularity/asm/ModuleAnnotation.java
@@ -1,11 +1,33 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Cube Island
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package de.cubeisland.engine.modularity.asm;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import com.sun.xml.internal.ws.org.objectweb.asm.Type;
 import de.cubeisland.engine.modularity.asm.ASMModuleParser.AnnotationType;
+import org.objectweb.asm.Type;
 
 public class ModuleAnnotation
 {
@@ -27,8 +49,7 @@ public class ModuleAnnotation
 
     public ModuleAnnotation(Type type, AnnotationType subtype, ModuleAnnotation parent)
     {
-        this.type = subtype;
-        this.annotationClass = type;
+        this(type, subtype, parent.type.getDeclaringClass().getName());
     }
 
     public void addProperty(String name, Object value)
--- asm/src/main/java/de/cubeisland/engine/modularity/asm/ModuleAnnotationVisitor.java
+++ asm/src/main/java/de/cubeisland/engine/modularity/asm/ModuleAnnotationVisitor.java
@@ -1,3 +1,25 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Cube Island
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package de.cubeisland.engine.modularity.asm;
 
 import org.objectweb.asm.AnnotationVisitor;
@@ -7,25 +29,18 @@ public class ModuleAnnotationVisitor extends AnnotationVisitor
 {
     private final ASMModuleParser discoverer;
     private final boolean array;
-    private final String name;
     private final boolean isSubAnnotation;
 
     public ModuleAnnotationVisitor(ASMModuleParser discoverer)
     {
+        this(discoverer, false, false);
+    }
+
+    private ModuleAnnotationVisitor(ASMModuleParser discoverer, boolean array, boolean isSubAnnotation)
+    {
         super(Opcodes.ASM5);
         this.discoverer = discoverer;
-    }
-
-    public ModuleAnnotationVisitor(ASMModuleParser discoverer, String name)
-    {
-        this.array = true;
-        this.name = name;
-        discoverer.addAnnotationArray(name);
-    }
-
-    public ModuleAnnotationVisitor(ASMModuleParser discoverer, boolean isSubAnnotation)
-    {
-        this(discoverer);
+        this.array = array;
         this.isSubAnnotation = isSubAnnotation;
     }
 
@@ -44,14 +59,25 @@ public class ModuleAnnotationVisitor extends AnnotationVisitor
     @Override
     public AnnotationVisitor visitArray(String name)
     {
-        return new ModuleAnnotationVisitor(discoverer, name);
+        discoverer.addAnnotationArray(name);
+        return forArrayAnnotation();
+    }
+
+    private AnnotationVisitor forArrayAnnotation()
+    {
+        return new ModuleAnnotationVisitor(discoverer, true, false);
     }
 
     @Override
     public AnnotationVisitor visitAnnotation(String name, String desc)
     {
         discoverer.addSubAnnotation(name, desc);
-        return new ModuleAnnotationVisitor(discoverer, true);
+        return forSubAnnotation();
+    }
+
+    private ModuleAnnotationVisitor forSubAnnotation()
+    {
+        return new ModuleAnnotationVisitor(discoverer, false, true);
     }
 
     @Override
