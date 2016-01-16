2bd4849d14318c91774d99336cb3a75e323d5f6b
Phillip Schichtel <phillip.public@schich.tel>
2016-01-17 00:39:47 +0100

    a simple search-and-replace based modulizer

--- modulizer/src/main/java/org/cubeengine/maven/plugin/modulizer/ModulizeMojo.java
+++ modulizer/src/main/java/org/cubeengine/maven/plugin/modulizer/ModulizeMojo.java
@@ -25,33 +25,19 @@ package org.cubeengine.maven.plugin.modulizer;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
-import spoon.Launcher;
-import spoon.OutputType;
-import spoon.compiler.Environment;
-import spoon.compiler.SpoonCompiler;
-import spoon.processing.AbstractProcessor;
-import spoon.processing.ProcessingManager;
-import spoon.processing.Processor;
-import spoon.reflect.declaration.CtAnnotation;
-import spoon.reflect.declaration.CtAnnotationType;
-import spoon.reflect.declaration.CtClass;
-import spoon.reflect.declaration.CtElement;
-import spoon.reflect.factory.Factory;
-import spoon.reflect.reference.CtTypeReference;
-import spoon.support.QueueProcessingManager;
 
-import java.io.File;
+import java.io.*;
 import java.lang.annotation.Annotation;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 /**
- * @requiresDependencyResolution test
  * @goal modulize
+ * @phase generate-sources
  * @threadSafe false
  */
 @SuppressWarnings("JavaDoc")
@@ -80,7 +66,7 @@ public class ModulizeMojo extends AbstractMojo
      * @parameter
      * @required
      */
-    private Map<String, Object> annotationValues = Collections.emptyMap();
+    private Map<String, String> annotationValues = Collections.emptyMap();
 
     /**
      * @parameter default-value="${project.build.sourceDirectory}"
@@ -88,101 +74,134 @@ public class ModulizeMojo extends AbstractMojo
     private File sourceFolder;
 
     /**
-     * @parameter default-value="${project.build.directory}/generated-sources"
+     * @parameter default-value="${project.build.directory}/generated-sources/modulized-classes"
      */
     private File targetFolder;
+
+    /**
+     * @parameter default-value="${project.build.sourceEncoding}"
+     */
+    private String encoding = "UTF-8";
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException
     {
-        if (project.getPackaging().equals("pom")) {
-            getLog().info("Parent project, skipping...");
+        if (project.getPackaging().equalsIgnoreCase("pom"))
+        {
+            getLog().info("Aggregator project, skipping...");
             return;
         }
-        getLog().info("Modulizing class " + mainClass + " from " + sourceFolder);
-        Launcher l = new Launcher();
-        SpoonCompiler c = l.createCompiler();
-        c.addInputSource(sourceFolder);
-        c.setSourceClasspath(this.loadClasspath(System.getProperty("java.class.path").split(File.pathSeparator)));
-        if (!targetFolder.exists())
+
+        int splitAt = mainClass.lastIndexOf('.');
+        String mainClassName = mainClass;
+        String mainPackage = null;
+        if (splitAt != -1)
         {
-            if (!targetFolder.mkdirs())
-            {
-                throw new MojoFailureException("Failed to create the target folder!");
-            }
+            mainClassName = mainClass.substring(splitAt + 1);
+            mainPackage = mainClass.substring(0, splitAt);
         }
-        else if (targetFolder.exists() && !targetFolder.isDirectory())
+
+        String relativePath = mainClassName + ".java";
+        if (mainPackage != null)
         {
-            throw new MojoFailureException("The target directory seems to be a file!");
+            relativePath = mainPackage.replace('.', File.separatorChar) + File.separator + relativePath;
         }
-        c.setSourceOutputDirectory(targetFolder);
-        final Factory f = c.getFactory();
-        ProcessingManager p = new QueueProcessingManager(f);
-        ClassProcessor proc = new ClassProcessor(f, getLog());
-        p.addProcessor(proc);
-        Environment e = f.getEnvironment();
-        e.setNoClasspath(true);
-        e.setManager(p);
-        e.setLevel("ALL");
-        if (c.build())
+
+        File source = new File(sourceFolder, relativePath);
+
+        if (!source.exists() || !source.isFile())
         {
-            getLog().info("Successfully modulized " + proc.getTagged() + " classes to " + targetFolder + "!");
-            c.generateProcessedSourceFiles(OutputType.CLASSES);
+            throw new MojoFailureException("Source main class file does not exist or is not a file: " + source);
         }
-        else
+
+        String sourceCode;
+        try
         {
-            throw new MojoFailureException("Failed to build the source model!");
+            sourceCode = readSource(source, encoding);
         }
+        catch (IOException e)
+        {
+            throw new MojoFailureException("Failed to read source file!", e);
+        }
+
+
+        File target = new File(targetFolder, relativePath);
+
+        try
+        {
+            writeTarget(target, encoding, addAnnotationToClass(sourceCode, mainClassName, constructAnnotation(annotationType, annotationValues)));
+        }
+        catch (IOException e)
+        {
+            throw new MojoFailureException("Failed to write the target!", e);
+        }
+
+        getLog().info("Class " + mainClass + " successfully modulized to " + target);
+        project.addCompileSourceRoot(target.toString());
+
     }
 
-    private String[] loadClasspath(String[] classpathEntries)
+    public static String addAnnotationToClass(String sourceCode, String className, String annotationCode)
     {
-        List<String> classpath = new ArrayList<String>(classpathEntries.length);
-        for (String entry : classpathEntries)
-        {
-            File file = new File(entry);
+        return sourceCode.replaceFirst("public\\s+class\\s+" + className, annotationCode + " $0");
+    }
 
-            if (!file.exists())
+    public static String constructAnnotation(String annotationType, Map<String, String> annotationValues)
+    {
+        StringBuilder annotationBuilder = new StringBuilder("@");
+        annotationBuilder.append(annotationType).append('(');
+
+        String separator = "";
+        for (Map.Entry<String, String> entry : annotationValues.entrySet())
+        {
+            annotationBuilder.append(separator).append(entry.getKey()).append(" = ").append(entry.getValue());
+            separator = ", ";
+        }
+
+        annotationBuilder.append(')');
+        return annotationBuilder.toString();
+    }
+
+    public static String readSource(File source, String charset) throws IOException
+    {
+        FileInputStream in = new FileInputStream(source);
+        byte[] data = new byte[(int) source.length()];
+        try
+        {
+            in.read(data);
+        }
+        finally
+        {
+            in.close();
+        }
+
+        return new String(data, charset);
+    }
+
+    public static void writeTarget(File target, String charset, String sourceCode) throws MojoFailureException, IOException
+    {
+        File targetDir = target.getParentFile();
+        if (!targetDir.exists())
+        {
+            if (!targetDir.mkdirs())
             {
-                getLog().warn("The classpath entry '" + entry + "' was removed. It doesn't exist.");
-                continue;
-            }
-
-            classpath.add(entry);
-        }
-
-        if (classpath.isEmpty())
-        {
-            getLog().warn("The classpath is empty.");
-        }
-
-        return classpath.toArray(new String[classpath.size()]);
-    }
-
-    private class ClassProcessor extends AbstractProcessor<CtClass> {
-        private final Factory f;
-        private final Log log;
-        private int tagged = 0;
-
-        public ClassProcessor(Factory f, Log log) {
-            this.f = f;
-            this.log = log;
-        }
-
-        public int getTagged() {
-            return tagged;
-        }
-
-        @Override
-        public void process(CtClass clazz)
-        {
-            log.info(clazz.getQualifiedName());
-            if (clazz.getQualifiedName().equals(mainClass)) {
-                CtAnnotation<Annotation> a = f.Core().createAnnotation();
-                a.setAnnotationType(f.Type().<Annotation>createReference(annotationType));
-                a.setElementValues(annotationValues);
-                clazz.addAnnotation(a);
+                throw new MojoFailureException("Failed to create the target directory: " + targetDir);
             }
         }
+        else if (!targetDir.isDirectory())
+        {
+            throw new MojoFailureException("The target directory exists, but is not a directory: " + targetDir);
+        }
+
+        FileOutputStream out = new FileOutputStream(target);
+        try
+        {
+            out.write(sourceCode.getBytes(charset));
+        }
+        finally
+        {
+            out.close();
+        }
     }
+
 }
